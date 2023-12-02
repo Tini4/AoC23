06c99607d82f37e323465629129169d33f941648
Tilen Juričan <tilen.jurican@gmail.com>
2023-12-02 16:35:13 +0100

    Put day 2 into functions

--- d2.py
+++ d2.py
@@ -1,30 +1,49 @@
+import operator
 import re
+from functools import reduce
+from typing import Iterable
 
 from main import fetch_lines
 
 
-lines = fetch_lines('d2.txt')
+def first(lines: Iterable[str]) -> int:
+    limit = {'red': 12, 'green': 13, 'blue': 14}
+    res = 0
 
-res = 0
-for line in lines:
-    if not line:
-        continue
+    for line in lines:
+        m = re.search(r'Game ([0-9]*): (.*)', line)
+        id_ = int(m.group(1))
+        draws = re.split(r';', m.group(2))
 
-    #print(line.strip())
+        for draw in draws:
+            for color in limit:
+                m = re.search(f'([0-9]*) {color}', draw)
+                if m and int(m.group(1)) > limit[color]:
+                    break
+            else:
+                continue
+            break
+        else:
+            res += id_
+    return res
 
-    m = re.search(r'Game [0-9]*: (.*)', line)
-    draws = re.split(r';', m.group(1))
-    bb = {'red': 0, 'green': 0, 'blue': 0}
-    for draw in draws:
-        r = re.search(r'([0-9]*) red', draw)
-        if r:
-            bb['red'] = max(int(r.group(1)), bb['red'])
-        g = re.search(r'([0-9]*) green', draw)
-        if g:
-            bb['green'] = max(int(g.group(1)), bb['green'])
-        b = re.search(r'([0-9]*) blue', draw)
-        if b:
-            bb['blue'] = max(int(b.group(1)), bb['blue'])
 
-    res += bb['red']*bb['green']*bb['blue']
-print(res)
+def second(lines: Iterable[str]) -> int:
+    res = 0
+
+    for line in lines:
+        m = re.search(r'Game [0-9]*: (.*)', line)
+        draws = re.split(r';', m.group(1))
+
+        colors = {'red': 0, 'green': 0, 'blue': 0}
+        for draw in draws:
+            for color in colors:
+                m = re.search(f'([0-9]*) {color}', draw)
+                if m:
+                    colors[color] = max(int(m.group(1)), colors[color])
+        res += reduce(operator.mul, colors.values())
+    return res
+
+
+print(first(fetch_lines('d2.txt')) == 2913)
+print(second(fetch_lines('d2.txt')) == 55593)
