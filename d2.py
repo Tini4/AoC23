76105f26a60c05c3411b7f089c23d121179bd437
Tilen Juričan <tilen.jurican@gmail.com>
2023-12-02 17:59:11 +0100

    Fix duplicate code in day 2

--- d2.py
+++ d2.py
@@ -1,36 +1,12 @@
 import operator
 import re
 from functools import reduce
-from typing import Iterable
+from typing import Iterable, Iterator
 
 from main import fetch_lines
 
 
-def first(lines: Iterable[str]) -> int:
-    limit = {'red': 12, 'green': 13, 'blue': 14}
-    res = 0
-
-    for line in lines:
-        m = re.search(r'Game ([0-9]*): (.*)', line)
-        id_ = int(m.group(1))
-        draws = re.split(r';', m.group(2))
-
-        for draw in draws:
-            for color in limit:
-                m = re.search(f'([0-9]*) {color}', draw)
-                if m and int(m.group(1)) > limit[color]:
-                    break
-            else:
-                continue
-            break
-        else:
-            res += id_
-    return res
-
-
-def second(lines: Iterable[str]) -> int:
-    res = 0
-
+def minimum(lines: Iterable[str]) -> Iterator[dict[str, int]]:
     for line in lines:
         m = re.search(r'Game [0-9]*: (.*)', line)
         draws = re.split(r';', m.group(1))
@@ -41,7 +17,27 @@ def second(lines: Iterable[str]) -> int:
                 m = re.search(f'([0-9]*) {color}', draw)
                 if m:
                     colors[color] = max(int(m.group(1)), colors[color])
-        res += reduce(operator.mul, colors.values())
+        yield colors
+
+
+def first(lines: Iterable[str]) -> int:
+    limit = {'red': 12, 'green': 13, 'blue': 14}
+    res = 0
+
+    for i, game in enumerate(minimum(lines)):
+        for color in limit:
+            if limit[color] < game[color]:
+                break
+        else:
+            res += i + 1
+    return res
+
+
+def second(lines: Iterable[str]) -> int:
+    res = 0
+
+    for game in minimum(lines):
+        res += reduce(operator.mul, game.values())
     return res
 
 
