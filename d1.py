e0c2f97a4284cd41c35fbae3350f016eea5d4bca
Tilen Juričan <tilen.jurican@gmail.com>
2023-12-02 15:46:13 +0100

    Reformat day 1

--- d1.py
+++ d1.py
@@ -1,8 +1,7 @@
 import re
-
 from typing import Iterable
-from main import fetch_lines
 
+from main import fetch_lines
 
 str_to_int = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,
               'zero': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '0': 0}
